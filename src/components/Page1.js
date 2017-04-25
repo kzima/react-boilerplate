b036dd42ebeeaf09f4b8ad99d3b240e4a35b58d7
Kris Zima <kris.zima@thehaystackapp.com>
2017-04-26 07:30:24 +1000

    Initial commit

---
+++ src/components/Page1.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+export default class extends Component {
+  constructor(){
+    super();
+
+    this.state = {
+      value: 1
+    }
+  }
+
+  changed = event => {
+    this.setState({
+      value: event.target.valueAsNumber
+    });
+  }
+
+  render() {
+    return <div>
+      Page       <input type="number" value={this.state.value} onChange={this.changed} />
+    </div>   
+  }
+}
+
+
+
---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import { Provider } from 'react-redux';
+import store from './store';
+import { AppContainer } from 'react-hot-loader';
+
+const render = (Component) => {
+  ReactDOM.render(<AppContainer>
+    <Provider store={store}>
+      <Component />
+    </Provider>
+  </AppContainer>,
+    document.getElementById('root')
+  );
+};
+
+render(App);
+
+module.hot.accept('./App', () => {
+  render(App)
+});
+
+
